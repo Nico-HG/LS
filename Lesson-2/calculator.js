62575ed11bcc06e5fae26852dabc1cb3cffe5731
Nico <Nicojr1111@gmail.com>
2023-11-21 22:13:00 +0000

    fixed calc and added JSON

--- Lesson-2/calculator.js
+++ Lesson-2/calculator.js
@@ -1,40 +1,56 @@
-const rlSync = require('readline-sync');
-
 //ask the user for the first number
 //ask the user for the second number
 //ask the user for an operation to perform
 //peform the operation on the two numbers
 //print the result to the terminal
-
-console.log('Welcome to Calculator');
+const rlSync = require('readline-sync');
+const messages = require('./calculator_messages.json')
+const LANGUAGE = 'English';
 
 let number1;
 let number2;
 let operator;
+let onOff;
+
+function logText(key, extraData = '') {
+  console.log(messages[LANGUAGE][key] + extraData);
+}
+
+function calculate() {
+do {
+  number1 = Number(rlSync.question(logText('firstNumberInput')));
+} while ( Object.is(number1, NaN) || !number1 ) 
 
 do {
-  number1 = Number(rlSync.question("What is your first number?\n"));
-} while ( Object.is(number1, NaN) ) 
+  number2 = Number(rlSync.question(logText('secondNumberInput')));
+} while ( Object.is(number2, NaN) || !number2 ) 
 
 do {
-  number2 = Number(rlSync.question("What is your second number?\n"));
-} while ( Object.is(number2, NaN) ) 
-
-do {
-  operator = rlSync.question('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide\n');
+  operator = rlSync.question(logText('operatorInput'));
 } while (operator !== '1' && operator !== '2' && operator !== '3' && operator !== '4') 
 
 switch (operator) {
   case '1':
-    console.log(`The result is: ${number1 + number2}`);
+    logText('resultLog', (number1 + number2));
     break;
   case '2':
-    console.log(`The result is: ${number1 - number2}`);
+    logText('resultLog', (number1 - number2));
     break;
   case '3':
-    console.log(`The result is: ${number1 * number2}`);
+    logText('resultLog', (number1 * number2));
     break;
   case '4':
-    console.log(`The result is: ${number1 / number2}`);  
+    logText('resultLog', (number1 / number2));  
     break;
 }
+
+do {
+  onOff = rlSync.question(logText('askAgain'));
+} while (onOff !== '1' && onOff !== '2');
+}
+
+logText('greeting');
+
+do { 
+calculate() 
+} while(onOff === '1');
