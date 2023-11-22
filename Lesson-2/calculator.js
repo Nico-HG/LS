228d4bd77ff72e8d2bd78c70cf2d900f1788083e
Nico <Nicojr1111@gmail.com>
2023-11-22 01:05:40 +0000

    refactoring

--- Lesson-2/calculator.js
+++ Lesson-2/calculator.js
@@ -4,53 +4,47 @@
 //peform the operation on the two numbers
 //print the result to the terminal
 const rlSync = require('readline-sync');
-const messages = require('./calculator_messages.json')
-const LANGUAGE = 'English';
+const MESSAGES = require('./calculator_messages.json'), LANGUAGE = 'English';
+let calculatorContinue;
 
-let number1;
-let number2;
-let operator;
-let onOff;
-
-function logText(key, extraData = '') {
-  console.log(messages[LANGUAGE][key] + extraData);
+function logText(key, clear, extraData = '') {
+  if (clear) console.clear();
+  console.log(MESSAGES[LANGUAGE][key] + extraData);
 }
-
-function calculate() {
-do {
-  number1 = Number(rlSync.question(logText('firstNumberInput')));
-} while ( Object.is(number1, NaN) || !number1 ) 
-
-do {
-  number2 = Number(rlSync.question(logText('secondNumberInput')));
-} while ( Object.is(number2, NaN) || !number2 ) 
-
-do {
-  operator = rlSync.question(logText('operatorInput'));
-} while (operator !== '1' && operator !== '2' && operator !== '3' && operator !== '4') 
-
-switch (operator) {
-  case '1':
-    logText('resultLog', (number1 + number2));
-    break;
-  case '2':
-    logText('resultLog', (number1 - number2));
-    break;
-  case '3':
-    logText('resultLog', (number1 * number2));
-    break;
-  case '4':
-    logText('resultLog', (number1 / number2));  
-    break;
+function getNumber(promptMessageKey) {
+  let num;
+  do {
+    num = rlSync.question(logText(promptMessageKey, true));
+  } while (Object.is(Number(num), NaN) || !(num.trim()));
+  return Number(num);
 }
-
-do {
-  onOff = rlSync.question(logText('askAgain'));
-} while (onOff !== '1' && onOff !== '2');
+function getOperation(promptMessageKey) {
+  let inputForOperation;
+  do {
+    inputForOperation = rlSync.question(logText(promptMessageKey, true));
+  } while (inputForOperation !== '1' && inputForOperation !== '2' && inputForOperation !== '3' && inputForOperation !== '4'); 
+  return inputForOperation;
 }
-
-logText('greeting');
-
+function calculateAndReturnResult(number1, number2, operation) {
+  if (operation === '4' && number2 === 0) return 'Invalid';
+  switch (operation) {
+    case '1': return (number1 + number2);
+    case '2': return (number1 - number2);
+    case '3': return (number1 * number2);
+    case '4': return (number1 / number2);  
+  }
+}
+function calculatorStart() {
+  let number1, number2, operation, result;
+  number1 = getNumber("firstNumberInput");
+  number2 = getNumber("secondNumberInput");
+  operation = getOperation('operationInput');
+  result = calculateAndReturnResult(number1, number2, operation);
+  do {
+    logText('resultLog', true, result);
+    calculatorContinue = rlSync.question(logText('askAgain'));
+  } while (calculatorContinue !== '1' && calculatorContinue !== '2');
+}
 do { 
-calculate() 
-} while(onOff === '1');
+calculatorStart(); 
+} while(calculatorContinue === '1');
